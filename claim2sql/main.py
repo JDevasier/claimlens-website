110d550ed545614e4bf71931eefbbd8316f3988d
phuongannnh <phuongaaanh@yahoo.com>
2023-07-06 13:03:57 -0500

    styling changes

--- claim2sql/main.py
+++ claim2sql/main.py
@@ -4,7 +4,6 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import json
 
-
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -14,7 +13,7 @@ templates = Jinja2Templates(directory="templates")
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get("/fact-check.html", response_class=HTMLResponse)
+@app.get("/fact-check", response_class=HTMLResponse)
 async def read_page1(request: Request):
     return templates.TemplateResponse("fact-check.html", {"request": request})
 
@@ -25,8 +24,7 @@ async def submit_text(request: Request):
 
     user_input = data.get("text")
 
-    if user_input == "Test":
-        model_outputs = {
+    model_outputs = {
         "input_sentence":"Joe Biden voted for the Iraq War.",
         "frame":"Vote",
         "target":"Voted",
@@ -40,8 +38,5 @@ async def submit_text(request: Request):
         "congress_member":"Joe Biden",
         "member_id":"B000444"
     }
-        
-    else:
-        model_outputs = "Invalid input."
 
     return templates.TemplateResponse("results.html", {"request": request, "model_outputs": model_outputs})
--- claim2sql/static/js/fact-check.js
+++ claim2sql/static/js/fact-check.js
@@ -1,13 +1,25 @@
 function submitForm() {
     const userInput = document.getElementById("input-text").value;
-    fetch('/submit', {
-        method: 'POST',
+    if (userInput === "") {
+        alert("Please input a claim to check!");
+        return;
+    }
+
+    fetch("/submit", {
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
         },
-        body: JSON.stringify({ 'text': userInput }),
-    }).then(response => response.text())
-        .then(html => {
+        body: JSON.stringify({ text: userInput }),
+    })
+        .then((response) => response.text())
+        .then((html) => {
             document.getElementById("result-container").innerHTML = html;
+            $("html,body").animate(
+                {
+                    scrollTop: $("#input-text").offset().top,
+                },
+                "slow"
+            );
         });
 }
