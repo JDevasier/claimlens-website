54bc404b0651d4162c90d84831060ef246a2cba7
phuongannnh <phuongaaanh@yahoo.com>
2023-07-18 22:53:19 -0500

    hover change

--- claim2sql/main.py
+++ claim2sql/main.py
@@ -3,11 +3,14 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import json
+import wikipediaapi
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
+
+wiki = wikipediaapi.Wikipedia('idir_lab', 'en')
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
@@ -33,10 +36,58 @@ async def submit_text(request: Request):
             "Issue":{"start":20, "end":32},
             "Position":{"start":16, "end":19}
         },
-        "bill_title":"Iraq War Resolution",
-        "bill_id":"hjres114-107",
+        "bills":[
+            {
+                "bill_title":"Iraq War Resolution",
+                "bill_id":"hjres114-107",
+                "bill_summary":"A joint resolution to authorize the use of United States Armed Forces against Iraq."
+            }, 
+            {
+                "bill_title":"Gun Control Act",
+                "bill_id":"hjres114-110",
+                "bill_summary":"A bill to restrict the use of guns in the United States."
+            },
+            {
+                "bill_title":"Abortion Act",
+                "bill_id":"hjres114-130",
+                "bill_summary":"A bill to allow the use of abortion in the United States."
+            }
+        ],
         "congress_member":"Joe Biden",
         "member_id":"B000444"
     }
 
-    return templates.TemplateResponse("results.html", {"request": request, "model_outputs": model_outputs})
+
+    fe_definitions = {
+        "Agent": "The conscious entity, generally a person, that performs the voting decision on an Issue.",
+        "Issue": "The matter which the Agent has a positive or negative opinion about and either votes for or votes against.",
+        "Side": "The conscious entity, generally a person, that performs the voting decision on an Issue together with the Agent.",
+        "Position": "This FE identifies the position that the Agent takes on an Issue. The Agent either votes for or against.",
+        "Frequency": "The number of times that the Agent made the same voting decision on an Issue.",
+        "Time": "This FE identifies the time when the Agent performs the voting decision.",
+        "Place": "The location where the voting decision took place.",
+        "Support_rate": "The percentage of Agent's total votes, in which the Agent took the same position with a Side for a specific period of time."
+    }
+
+
+
+    # Build html for frame elements
+    prev_end = 0
+    fe_html = ""
+    for fe, span in sorted(model_outputs["frame_elements"].items(), key=lambda x: x[1]['start']):
+        if prev_end != span['start']:
+            fe_html += f"<span class='non-fe'>{model_outputs['input_sentence'][prev_end:span['start']]}</span>"
+        fe_html += f"<span data-content='{fe_definitions[fe]}' class='fe-{fe.lower()}'>{model_outputs['input_sentence'][span['start']:span['end']]}</span>"
+
+        prev_end = span['end']
+    fe_html += f"<span>{model_outputs['input_sentence'][prev_end:]}</span>"
+
+
+
+    # Get agent wikipedia page
+    agent_page = wiki.page(model_outputs["congress_member"])
+    agent_summary = agent_page.summary.split("\n")[0]
+
+    agent_url = agent_page.fullurl
+
+    return templates.TemplateResponse("results.html", {"request": request, "model_outputs": model_outputs, "agent_summary": agent_summary, "agent_url": agent_url, "fe_html": fe_html})
--- claim2sql/static/js/fact-check.js
+++ claim2sql/static/js/fact-check.js
@@ -1,30 +1,3 @@
-// function submitForm() {
-//     const userInput = document.getElementById("input-text").value;
-//     if (userInput === "") {
-//         alert("Please input a claim to check!");
-//         return;
-//     }
-
-//     fetch("/submit", {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({ text: userInput }),
-//     })
-//         .then((response) => response.text())
-//         .then((html) => {
-//             document.getElementById("results-container").innerHTML = html;
-//             $("html,body").animate(
-//                 {
-//                     scrollTop: $("#input-text").offset().top,
-//                 },
-//                 "slow"
-//             );
-//         });
-// }
-
-
 function submitForm() {
     const userInput = document.getElementById("input-text").value;
     if (userInput === "") {
@@ -41,73 +14,110 @@ function submitForm() {
     })
         .then((response) => response.text())
         .then((html) => {
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(html, "text/html");
-
-            // Get the input sentence element from the parsed HTML
-            const inputSentenceElement = doc.getElementById("inputSentence");
-
-            // Model outputs data
-            const model_outputs = {
-                input_sentence: "Joe Biden voted for the Iraq War.",
-                agent: { start: 0, end: 8, explanation: "Joe Biden is the agent." },
-                target: { start: 10, end: 14, explanation: "voted is the target." },
-                // frame_elements: {
-                //     Agent: { start: 0, end: 8, explanation: "Explanation for 'agent'" },
-                //     Issue: { start: 12, end: 16, explanation: "Explanation for 'issue'" },
-                //     Position: { start: 4, end: 6, explanation: "Explanation for 'position'" },
-                // },
-                position: { start: 16, end: 19, explanation: "for is the position." },
-                issue: { start: 20, end: 31, explanation: "the Iraq War is the issue." },
-            };
-
-            const sentenceText = model_outputs.input_sentence;
-            const elements = Object.keys(model_outputs).filter((key) => key !== "input_sentence" && key !== "frame_elements");
-            elements.sort((a, b) => model_outputs[a].start - model_outputs[b].start);
-
-            let sentenceHTML = "";
-            let lastIndex = 0;
-
-            elements.forEach(function (element, index) {
-                const { start, end, explanation } = model_outputs[element];
-                const before = sentenceText.substring(lastIndex, start);
-                const word = sentenceText.substring(start, end + 1);
-
-                // Generate a unique class name for each highlighted element
-                const className = `highlight-${index + 1}`;
-
-                sentenceHTML += `${before}<span class="highlight ${className}" onmouseover="showPopup(this);" onmouseout="hidePopup(this);">
-                    ${word}
-                    <span class="popup">${explanation}</span>
-                </span>`;
-
-                lastIndex = end + 1;
-            });
-
-            sentenceHTML += sentenceText.substring(lastIndex);
-
-            inputSentenceElement.innerHTML = sentenceHTML;
-
-            // Replace the result container's innerHTML with the modified HTML
-            document.getElementById("results-container").innerHTML = doc.documentElement.innerHTML;
-        })
-        .catch((error) => {
-            console.error("Error:", error);
+            document.getElementById("results-container").innerHTML = html;
+            $("html,body").animate(
+                {
+                    scrollTop: $("#input-text").offset().top,
+                },
+                "slow"
+            );
+            $('.fe-agent, .fe-issue, .fe-side, .fe-position, .fe-frequency, .fe-time, .fe-place, .fe-support_rate')
+                .popup({
+                    inline: true,
+                    hoverable: true,
+                    position: 'top center',
+                    delay: {
+                        show: 0,
+                        hide: 0
+                    }
+                });
         });
-    $("html,body").animate(
-        {
-            scrollTop: $("#input-text").offset().top,
-        },
-        "slow"
-    );
 }
 
-function showPopup(element) {
-    const popup = element.querySelector(".popup");
-    popup.style.visibility = "visible";
-}
 
-function hidePopup(element) {
-    const popup = element.querySelector(".popup");
-    popup.style.visibility = "hidden";
-}
+// function submitForm() {
+//     const userInput = document.getElementById("input-text").value;
+//     if (userInput === "") {
+//         alert("Please input a claim to check!");
+//         return;
+//     }
+
+//     fetch("/submit", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ text: userInput }),
+//     })
+//         .then((response) => response.text())
+//         .then((html) => {
+//             const parser = new DOMParser();
+//             const doc = parser.parseFromString(html, "text/html");
+
+//             // Get the input sentence element from the parsed HTML
+//             const inputSentenceElement = doc.getElementById("inputSentence");
+
+//             // Model outputs data
+//             const model_outputs = {
+//                 input_sentence: "Joe Biden voted for the Iraq War.",
+//                 agent: { start: 0, end: 8, explanation: "Joe Biden is the agent." },
+//                 target: { start: 10, end: 14, explanation: "voted is the target." },
+//                 // frame_elements: {
+//                 //     Agent: { start: 0, end: 8, explanation: "Explanation for 'agent'" },
+//                 //     Issue: { start: 12, end: 16, explanation: "Explanation for 'issue'" },
+//                 //     Position: { start: 4, end: 6, explanation: "Explanation for 'position'" },
+//                 // },
+//                 position: { start: 16, end: 19, explanation: "for is the position." },
+//                 issue: { start: 20, end: 31, explanation: "the Iraq War is the issue." },
+//             };
+
+//             const sentenceText = model_outputs.input_sentence;
+//             const elements = Object.keys(model_outputs).filter((key) => key !== "input_sentence" && key !== "frame_elements");
+//             elements.sort((a, b) => model_outputs[a].start - model_outputs[b].start);
+
+//             let sentenceHTML = "";
+//             let lastIndex = 0;
+
+//             elements.forEach(function (element, index) {
+//                 const { start, end, explanation } = model_outputs[element];
+//                 const before = sentenceText.substring(lastIndex, start);
+//                 const word = sentenceText.substring(start, end + 1);
+
+//                 // Generate a unique class name for each highlighted element
+//                 const className = `highlight-${index + 1}`;
+
+//                 sentenceHTML += `${before}<span class="highlight ${className}" onmouseover="showPopup(this);" onmouseout="hidePopup(this);">
+//                     ${word}
+//                     <span class="popup">${explanation}</span>
+//                 </span>`;
+
+//                 lastIndex = end + 1;
+//             });
+
+//             sentenceHTML += sentenceText.substring(lastIndex);
+
+//             inputSentenceElement.innerHTML = sentenceHTML;
+
+//             // Replace the result container's innerHTML with the modified HTML
+//             document.getElementById("results-container").innerHTML = doc.documentElement.innerHTML;
+//         })
+//         .catch((error) => {
+//             console.error("Error:", error);
+//         });
+//     $("html,body").animate(
+//         {
+//             scrollTop: $("#input-text").offset().top,
+//         },
+//         "slow"
+//     );
+// }
+
+// function showPopup(element) {
+//     const popup = element.querySelector(".popup");
+//     popup.style.visibility = "visible";
+// }
+
+// function hidePopup(element) {
+//     const popup = element.querySelector(".popup");
+//     popup.style.visibility = "hidden";
+// }
